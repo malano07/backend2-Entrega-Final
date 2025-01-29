a008b7fe1c082edd494f47ba4028eee6b7d877e6
malano07 <149729686+malano07@users.noreply.github.com>
2025-01-29 12:07:12 -0300

    Delete back end 2 entrega final directory

--- back end 2 entrega final/src/routes/products.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
-
-const router = Router();
-
-router.get("/products", getAllProducts);
-
-export default router;
